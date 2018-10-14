a02ecf411ee2f0a70294e5c6f842b8eb6dfb5b3d
falsy <me@falsy.me>
2018-10-14 19:31:44 +0900

    v3.0.5
    * 로고 및 파비콘 수정

--- wp-content/themes/Cheolguso/header.php
+++ wp-content/themes/Cheolguso/header.php
@@ -15,8 +15,8 @@
 <!--[if lt IE 9]>
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
-<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon_v2.ico" type="image/x-icon">
-<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon_v2.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon_v3.ico" type="image/x-icon">
+<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon_v3.ico" type="image/x-icon">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no" />
 <?php wp_head(); ?>
 
--- wp-content/themes/Cheolguso/single.php
+++ wp-content/themes/Cheolguso/single.php
@@ -2,7 +2,7 @@
 <div id="post-<?php the_ID() ; ?>" <?php post_class('overview'); ?>>
   <?php while ( have_posts() ) : the_post(); ?>
   <header>
-    <div id="home"><a href="<?php echo home_url(); ?>"><img src="https://lab.falsy.me/wp-content/uploads/2018/09/falsylab-icon.png" alt="cheolguso logo" width="24"/></a></div>
+    <div id="home"><a href="<?php echo home_url(); ?>"><img src="https://lab.falsy.me/wp-content/uploads/2018/10/falsylab-icon-v2.png" alt="cheolguso logo" width="24"/></a></div>
     <div class="container">
       <figure>
         <?php the_post_thumbnail(); ?>
