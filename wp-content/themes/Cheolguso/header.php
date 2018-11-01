52d5b487c73410c667f274dccfd432e418b10357
falsy <me@falsy.me>
2018-11-02 00:39:05 +0900

    파비콘 수정 및 사이트 이미지 추가

--- wp-content/themes/Cheolguso/header.php
+++ wp-content/themes/Cheolguso/header.php
@@ -15,9 +15,20 @@
 <!--[if lt IE 9]>
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
-<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon_v3.ico" type="image/x-icon">
-<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon_v3.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon_v4.ico" type="image/x-icon">
+<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon_v4.ico" type="image/x-icon">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+<meta name="author" content="falsy">
+<meta property="og:site_name" content="폴시랩">
+<meta property="og:url" content="https://lab.falsy.me">
+<meta property="og:type" content="website">
+<meta property="og:title" content="폴시랩">
+<meta property="og:description" content="자바스트립트, PHP, 워드프레스, HTML, CSS, 팁, 가이드, 일상이야기.">
+<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/logo1200x630.png">
+<meta name="twitter:title" content="폴시랩">
+<meta name="twitter:description" content="자바스트립트, PHP, 워드프레스, HTML, CSS, 팁, 가이드, 일상이야기.">
+<meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/img/logo1200x630.png">
+
 <?php wp_head(); ?>
 
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
