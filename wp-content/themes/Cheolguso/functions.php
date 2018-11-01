e30534aecf97f24e79733c62115f2522c56f9f9b
falsy <me@falsy.me>
2018-11-01 23:50:04 +0900

    모바일 메뉴 스타일 수정

--- wp-content/themes/Cheolguso/functions.php
+++ wp-content/themes/Cheolguso/functions.php
@@ -4,7 +4,7 @@
 function CGS_scripts_styles() {
   wp_enqueue_style( 'cgs_proxima_nova', get_template_directory_uri() . '/css/cgs_proxima_nova.css', false, '1.0.0');
   wp_enqueue_style( 'cgs_style', get_template_directory_uri() . '/css/style.css', false, '1.1.0');
-  wp_enqueue_style( 'cgs_responsive', get_template_directory_uri() . '/css/responsive_style.css', false, '1.1.0');
+  wp_enqueue_style( 'cgs_responsive', get_template_directory_uri() . '/css/responsive_style.css', false, '1.1.1');
 }
 add_action( 'wp_enqueue_scripts', 'CGS_scripts_styles' );
 
