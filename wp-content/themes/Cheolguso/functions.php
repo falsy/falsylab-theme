b6fe5174c6ade73a29b0480bd6186b0c8ec6bbcc
falsy <me@falsy.me>
2018-11-03 20:32:24 +0900

    소개 페이지 컨텐츠 추가

--- wp-content/themes/Cheolguso/functions.php
+++ wp-content/themes/Cheolguso/functions.php
@@ -3,7 +3,7 @@
 //enqueue style & script
 function CGS_scripts_styles() {
   wp_enqueue_style( 'cgs_proxima_nova', get_template_directory_uri() . '/css/cgs_proxima_nova.css', false, '1.0.0');
-  wp_enqueue_style( 'cgs_style', get_template_directory_uri() . '/css/style.css', false, '1.1.0');
+  wp_enqueue_style( 'cgs_style', get_template_directory_uri() . '/css/style.css', false, '1.1.1');
   wp_enqueue_style( 'cgs_responsive', get_template_directory_uri() . '/css/responsive_style.css', false, '1.1.3');
 }
 add_action( 'wp_enqueue_scripts', 'CGS_scripts_styles' );
