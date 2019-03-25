98729b10b0128765cbcc09824a3c8b68caea9206
falsy <me@falsy.me>
2019-03-26 07:16:16 +0900

    인피니티 스크롤 수정2

--- wp-content/themes/Cheolguso/index.php
+++ wp-content/themes/Cheolguso/index.php
@@ -9,7 +9,7 @@
       if ( have_posts() ) {
         while ( have_posts() ) {
           the_post();
-          get_template_part();
+          get_template_part('template-parts');
         }
       }
       ?>
