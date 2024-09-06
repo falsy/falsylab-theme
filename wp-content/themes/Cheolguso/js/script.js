b7708fa691d478a1fbd26bec1db307e2ea0cec36
CheolJe <me@falsy.me>
2024-09-06 16:33:42 +0900

    feat: 불필요한 코드 제거 및 화면 모드 설정 변경

--- wp-content/themes/Cheolguso/js/script.js
+++ wp-content/themes/Cheolguso/js/script.js
@@ -1,18 +1,28 @@
-jQuery(document).ready(function($) {
-	'use strict';
+jQuery(document).ready(function ($) {
+  "use strict"
 
-	$('.sun').click(function() {
-		$('body').removeClass('dark-mode');
-		document.cookie = "dark_mode=false; path=/; max-age=0; domain=falsy.me; secure";
-		document.cookie = "light_mode=true; path=/; max-age=1036800; domain=falsy.me; secure";
-	});
+  $(".sun").click(function () {
+    $("body").removeClass("dark-mode")
+    document.cookie =
+      "dark_mode=false; path=/; max-age=0; domain=falsy.me; secure"
+    document.cookie =
+      "light_mode=true; path=/; max-age=1036800; domain=falsy.me; secure"
+  })
 
-	$('.moon').click(function() {
-		$('body').addClass('dark-mode');
-		document.cookie = "dark_mode=true; path=/; max-age=1036800; domain=falsy.me; secure";
-		document.cookie = "light_mode=false; path=/; max-age=0; domain=falsy.me; secure";
-	});
+  $(".moon").click(function () {
+    $("body").addClass("dark-mode")
+    document.cookie =
+      "light_mode=false; path=/; max-age=0; domain=falsy.me; secure"
+    document.cookie =
+      "dark_mode=true; path=/; max-age=1036800; domain=falsy.me; secure"
+  })
 
-	const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: Dark)').matches;
-	if(isDarkMode && document.cookie.indexOf('light_mode') === -1) $('body').addClass('dark-mode');
-});
+  if (
+    document.cookie.indexOf("light_mode") === -1 &&
+    document.cookie.indexOf("dark_mode") === -1 &&
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: Dark)").matches
+  ) {
+    $("body").addClass("dark-mode")
+  }
+})
