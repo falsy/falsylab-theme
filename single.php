4deee40e8b0a11a7a6ad2e70acbb0974863d3635
CheolJe <me@falsy.me>
2026-04-26 20:08:35 +0900

    fix: 댓글 수정

--- single.php
+++ single.php
@@ -85,7 +85,7 @@
     ?>
 
     <script src="https://utteranc.es/client.js"
-        repo="falsy/falsy.me"
+        repo="falsy/falsylab-theme"
         issue-term="pathname"
         theme="github-light"
         crossorigin="anonymous"
