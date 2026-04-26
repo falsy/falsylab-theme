c897abacfa01749d404c8077297816547e7b7cf0
CheolJe <me@falsy.me>
2026-04-26 19:58:06 +0900

    feat: 광고 추가

--- single.php
+++ single.php
@@ -68,7 +68,7 @@
       </div>
     </div>
 
-    <!-- <div class="footer-google">
+    <div class="footer-google">
       <p class="ad-title">Advertisement</p>
       <ins class="adsbygoogle"
            style="display:block"
@@ -78,10 +78,7 @@
       <script>
       (adsbygoogle = window.adsbygoogle || []).push({});
       </script>
-      <div class="donate-text">
-        <p>All ad revenue is donated to local children in need.</p>
-      </div>
-    </div>  -->
+    </div> 
 
     <?php 
       // comments_template(); 
